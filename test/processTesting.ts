cbaadb7ca137ee13a5f2c9b3c6c112dfffb8160d
zhangwenfei <blackness@126.com>
2020-05-01 17:51:56 +0800

    pref: until两个形参位置调换(count, done -> done, count)

--- test/processTesting.ts
+++ test/processTesting.ts
@@ -1,4 +1,5 @@
 /** 整体流程测试 */
+
 import { AsyncManager } from '../src/index';
 
 const asyncManager = new AsyncManager();
@@ -24,7 +25,7 @@ function tUntil() {
     // 不return阻断 手动执行成功
     // 期望执行成功回调并且不再执行until函数
     let uCount = 0;
-    asyncManager.until(function(count: number, done: any) {
+    asyncManager.until(function(done: any, count: number) {
         uCount = count;
         if (count === 5) {
             done('success');
@@ -45,7 +46,7 @@ function tUntil() {
 
     // 不设置最大重试次数
     // 期望执行200次后内部抛出错误
-    asyncManager.until(function(count: number, done: any) {
+    asyncManager.until(function(done: any, count: number) {
         if (count >= 200) {
             throw new Error('达到设定执行上限');
         }
@@ -62,7 +63,7 @@ function tUntil() {
 
     // 不设置最大重试次数
     // 期望捕获到栈溢出的错误
-    asyncManager.until(function(count: number, done: any) {
+    asyncManager.until(function(done: any) {
         done.again();
     }).then(function() {
         logError('until', '[4]不该执行这里！')
@@ -78,7 +79,7 @@ function tUntil() {
     // 并且不手动执行 成功/失败 行为
     // 期望在执行了指定次数后抛出错误
     uCount = 0;
-    asyncManager.until(function(count: number, done: any) {
+    asyncManager.until(function(done: any, count: number) {
         uCount = count;
         done.again();
     }, 5).then(function() {
@@ -93,7 +94,111 @@ function tUntil() {
 
     // 设置最大重试次数
     // 手动执行成功
-    // 期望
+    // 期望进入成功回调
+    asyncManager.until(function(done: any, count: number) {
+        done(count);
+    }, 10).then(function(res: any) {
+        if (res === 1) {
+            logSuccess('until', '[6]成功')
+        } else {
+            logError('until', '[6]错误的结果：' + res)
+        }
+    }).catch(function(err: any) {
+        logError('until', '[6]不该执行这里：' + err.message);
+    })
+
+    // [7]done -> done.break -> done.again
+    // 期望只有done执行成功
+    asyncManager.until(function(done: any, count: number) {
+        done(count);
+        done.break();
+        done.again();
+        if (count > 1) {
+            // count不能大于1因为期望执行一次
+            logError('until', '[7]函数执行多次');
+        }
+    }).then(function(res: any) {
+        if (res === 1) {
+            logSuccess('until', '[7]成功');
+        } else {
+            logError('until', '[7]错误的执行次数：' + res);
+        }
+    }).catch(function(err: any) {
+        logError('until', '[7]不该执行这里：' + err.message);
+    })
+
+    // [8]done.break -> done.again -> done
+    // 期望捕获错误
+    asyncManager.until(function(done: any, count: number) {
+        done.break(count);
+        done.again();
+        done('nothing');
+        if (count > 1) {
+            logError('until', '[8]函数执行多次');
+        }
+    }).then(function(res: any) {
+        logError('until', '[8]不该执行这里：' + res)
+    }).catch(function(err: any) {
+        if (err === 1) {
+            logSuccess('until', '[8]成功')
+        } else {
+            logError('until', '[8]错误信息异常：' + err.message)
+        }
+    })
+
+    // [9]done.again -> done -> done.break
+    // catch 、 then
+    // 期望捕获栈溢出的错误
+    asyncManager.until(function(done: any, count: number) {
+        done.again();
+        done(count);
+        done.break(count);
+    }).catch(function(err: any) {
+        if (err.message === 'Maximum call stack size exceeded') {
+            logSuccess('until', '[9]成功')
+        } else {
+            logError('until', '[9]错误信息异常：' + err.message)
+        }
+    }).then(function() {
+        logError('until', '[9]不该执行这里！');
+    })
+
+    // [10]在then中抛出错误
+    // 期望：catch中抛出
+    let timer2: any;
+    asyncManager.until(function(done: any) {
+        done();
+        timer2 = setTimeout(function() {
+            logError('until', '[10]没有触发回调！')
+        }, 0);
+    }).then(function() {
+        throw new Error('报错了');
+    }).catch(function(err: any) {
+        clearTimeout(timer2);
+        if (err.message === '报错了') {
+            logSuccess('until', '[10]成功')
+        } else {
+            logError('until', '[10]捕获到错误，但是错误信息不对：' + err.message);
+        }
+    })
+
+    // [11]then中传入Promise
+    // 期望：最后一个then中拿到Promise解析完后的值
+    asyncManager.until(function(done: any) {
+        done();
+    }).then(function() {
+        return new Promise((resolve, reject) => {
+            resolve(1)
+        })
+    }).then(function(res: any) {
+        if (res === 1) {
+            logSuccess('until', '[11]成功')
+        } else {
+            logError('until', '[11]返回值不对：' + res);
+        }
+    }).catch(function(err: any) {
+        logError('until', '[11]不该执行这里！' + err.message)
+    })
 }
 
 function logSuccess(module: string, message: string) {
@@ -107,18 +212,5 @@ function logError(module: string, message: string) {
 tUntil();
 
 
-
-
-
-
-
-
-
-
-
-
-
-/**
- * 已知的问题：
- * 1、两个实例并行执行
- */
+// TODO catch之后可以再进入then
+// then之后删除自身
